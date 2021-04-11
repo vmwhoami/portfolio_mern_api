1956755997c1273b82b082f0b7a6cf626cfa98b1
Vitalie Melnic <vmwhoami@gmail.com>
2021-04-11 20:23:23 +0300

    Add create user possibility

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
 exports.getAllUsers = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -9,10 +12,30 @@ exports.getAllUsers = (req, res) => {
 };
 
 exports.createUser = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    timeOfUnswer: res.timpulCerrerii,
-  });
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({
+          email: "A user has already been regitered with this email"
+        })
+      } else {
+        const newUser = new User({
+          handle: req.body.handle,
+          email: req.body.email,
+          password: req.body.password
+        })
+      }
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, err, (err, hash) => {
+          if (err) throw (err);
+          newUser.password = hash;
+          newUser.save().then(user => res.json(user))
+            .catch(err => console.log(err));
+        })
+      })
+
+
+    })
 };
 
 exports.getUser = (req, res) => {
--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 const router = express.Router();
 const {
   getAllUsers,
