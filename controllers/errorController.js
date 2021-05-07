ac2601efc421ef4578ed0acb5521b84f9bd2d326
vmwhoami <vmwhoami@gmail.com>
2021-05-07 16:35:50 +0300

    Handle error in production and development

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,33 @@
+
+const sendErrorProduction = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('Error', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
 /* eslint-disable */
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+
+  } else {
+    sendErrorProduction(err, res)
+  }
+
 };
